e483ccbe5354dd61ae85eb7dbf10f70699776975
SabithHaneef <sabith.th.07@gmail.com>
2018-10-19 21:55:36 +0530

    added express to serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Prisma } from './generated/prisma'
-import resolvers from './resolvers'
+import * as express from "express";
+import { GraphQLServer } from "graphql-yoga";
+import { Prisma } from "./generated/prisma";
+import resolvers from "./resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
-    }),
-  }),
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+    })
+  })
+});
+
+server.express.use("/images", express.static("images"));
+server.start(() => console.log(`Server is running on http://localhost:4000`));
