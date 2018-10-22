b4e8fc7f3933ba51188c91b500cf7ea29c830015
SabithHaneef <sabith.th.07@gmail.com>
2018-10-22 18:58:52 +0530

    added productsConnection query to support pagination

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -7,6 +7,11 @@ export const Query = {
     return forwardTo("db")(parent, args, ctx, info);
   },
 
+  productsConnection: (parent, args, ctx: Context, info) => {
+    getUserId(ctx);
+    return forwardTo("db")(parent, args, ctx, info);
+  },
+
   feed(parent, args, ctx: Context, info) {
     return ctx.db.query.posts({ where: { isPublished: true } }, info);
   },
